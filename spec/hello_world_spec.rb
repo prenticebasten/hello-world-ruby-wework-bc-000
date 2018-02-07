fe7824914bc96a9228849586f93acc07e4421096
Prentice Basten <prentice.basten@wework.com>
2018-02-07 03:19:11 +0000

    Done.

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -3,7 +3,7 @@ describe "Hello World" do
     expect(File.exists?("hello_world.rb")).to be_truthy, "Did you create the file hello_world.rb? (If so, is it spelled exactly the same with the same capitalization?)"
   end
 
-  it 'puts "Hello World!"' do
+  it 'puts "Hello World! "' do
     # $stdout represents the output of your program, don't worry about how
     # we are testing this, focus on simply coding your solution.
     expect($stdout).to receive(:puts).with("Hello World!")
